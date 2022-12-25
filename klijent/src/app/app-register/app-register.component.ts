da1ca3dfbac9113afeba9dc0949352b3e354a422
ilicandjela <mladostmidji@gmail.com>
2022-12-25 14:01:47 +0100

    imported forms

--- klijent/src/app/app-register/app-register.component.ts
+++ klijent/src/app/app-register/app-register.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-app-register',
   templateUrl: './app-register.component.html',
   styleUrls: ['./app-register.component.css']
 })
-export class AppRegisterComponent implements OnInit {
+export class AppRegisterComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public registerForm: FormGroup;
+  public returnUrl: string;
+   
+
+  constructor(private formBuilder: FormBuilder,
+              private router: Router,
+              private route: ActivatedRoute,
+    ) { 
+      this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || 'home';
+    }
 
   ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      ime : new FormControl('', [Validators.required]),
+      prezime : new FormControl('', [Validators.required]),
+      JMBG : new FormControl('', [Validators.required, Validators.pattern('^[0-9]{13}$'), Validators.minLength(13), Validators.maxLength(13)]),
+      telefon : new FormControl('', [Validators.required, Validators.minLength(7)])
+  });
+
   }
 
+  ngOnDestroy(): void {
+    
+  }
 }
--- klijent/src/app/app.module.ts
+++ klijent/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppRegisterComponent } from './app-register/app-register.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { AppRegisterComponent } from './app-register/app-register.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
