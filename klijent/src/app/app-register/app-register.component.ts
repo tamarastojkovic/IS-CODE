0e23ac004a4b668c37931569ef494989f9175275
ilicandjela <mladostmidji@gmail.com>
2022-12-25 13:48:44 +0100

    client app

---
+++ klijent/src/app/app-register/app-register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-register',
+  templateUrl: './app-register.component.html',
+  styleUrls: ['./app-register.component.css']
+})
+export class AppRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- klijent/src/app/app.module.ts
+++ klijent/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRegisterComponent } from './app-register/app-register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppRegisterComponent
   ],
   imports: [
     BrowserModule,
