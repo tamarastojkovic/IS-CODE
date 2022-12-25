e4154b27ba3d880c82355632fab855ca52c309db
Tamara Stojkovic <tamara.stojkovic.1998@gmail.com>
2022-12-25 17:33:01 +0100

    Dodate rute

--- klijent/src/app/app-routing.module.ts
+++ klijent/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppRegisterComponent } from './app-register/app-register.component'
+import { CandidateProfileComponent } from './candidate-profile/candidate-profile.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+    {path: 'register', component: AppRegisterComponent },
+    {path: 'login', component: LoginComponent },
+    {path: 'profile', component:CandidateProfileComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- klijent/src/app/app.module.ts
+++ klijent/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppRegisterComponent } from './app-register/app-register.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { CandidateProfileComponent } from './candidate-profile/candidate-profile.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
